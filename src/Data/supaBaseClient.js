9f2b6c37c3852f840c478246c7068425b75bec7f
Marcellinusadiwijaya <cellinusadi@gmail.com>
2026-06-11 17:52:52 +0700

    Delete src/Data/supaBaseClient.js

--- src/Data/supaBaseClient.js
+++
@@ -1,9 +0,0 @@
-// src/lib/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-// Ganti dengan URL dan Anon Key dari project Supabase kamu
-// Bisa ditemukan di: https://supabase.com/dashboard → Project → Settings → API
-const SUPABASE_URL = "https://txxzhxejewvrgdscgngk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHpoeGVqZXd2cmdkc2NnbmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMzMxMTMsImV4cCI6MjA5NDkwOTExM30.IMKauvepRbLbctMxYuWriB4uVRIIXC6EZLosPa4GNo4";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
